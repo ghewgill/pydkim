dd031018e64fbad0b0c6c0a714e5987ff68db7c2
greg <greg@138defff-d0dd-dc11-a92e-0011d808431b>
2008-02-18 09:36:10 +0000

    add command line parameters
    git-svn-id: file:///home/svn/pydkim/trunk@3 138defff-d0dd-dc11-a92e-0011d808431b

--- dkimsign.py
+++ dkimsign.py
@@ -4,9 +4,18 @@ import sys
 
 import dkim
 
+if len(sys.argv) < 4 or len(sys.argv) > 5:
+    print >>sys.stderr, "Usage: dkimsign.py selector domain privatekeyfile [identity]"
+    sys.exit(1)
+
+selector = sys.argv[1]
+domain = sys.argv[2]
+privatekeyfile = sys.argv[3]
+identity = len(sys.argv) >= 5 and sys.argv[4]
+
 message = sys.stdin.read()
 try:
-    sig = dkim.sign(message, "greg", "hewgill.com", open("/home/greg/.domainkeys/rsa.private", "r").read())
+    sig = dkim.sign(message, selector, domain, open(privatekeyfile, "r").read(), identity = identity)
     sys.stdout.write(sig)
     sys.stdout.write(message)
 except Exception, e:
